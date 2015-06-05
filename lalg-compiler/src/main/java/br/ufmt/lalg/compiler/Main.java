15f04814b3055c17f027982ca855a9869bbae524
Alvaro Viebrantz <alvarowolfx@gmail.com>
2015-06-04 23:39:28 -0400

    Add CLI to compile lalg files

--- lalg-compiler/src/main/java/br/ufmt/lalg/compiler/Main.java
+++ lalg-compiler/src/main/java/br/ufmt/lalg/compiler/Main.java
@@ -1,20 +1,51 @@
 package br.ufmt.lalg.compiler;
 
+import br.ufmt.lalg.compiler.lexico.AnalisadorLexico;
+import br.ufmt.lalg.compiler.lexico.AnaliseLexicaException;
+import br.ufmt.lalg.compiler.sintatico.AnalisadorSintatico;
+import br.ufmt.lalg.compiler.sintatico.AnaliseSintaticaException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class Main {
 
+    public static final String USAGE = "Usage: java -jar lalg-compiler file_name.lalg";
 
-	public static void main(String[] args) {		
-		/*
-		SwingUtilities.invokeLater(new Runnable() {
-			
-			public void run() {
+	public static void main(String[] args) {
+		if(args.length != 1){
+			System.out.println(USAGE);
+            System.exit(-1);
+		}
 
-				new PrincipalView();
-				
-			}
-		});
-		*/
+        String filename = args[0];
+        if(!filename.endsWith(".lalg")){
+            System.out.println("A .lalg file should be passed");
+            System.exit(-1);
+        }
+        try {
+            String conteudoArquivo = new String(Files.readAllBytes(Paths.get(filename)));
 
-	}
+            AnalisadorLexico analisadorLexico = new AnalisadorLexico(conteudoArquivo);
+            analisadorLexico.fazerAnalise();
+
+            AnalisadorSintatico analisadorSintatico = new AnalisadorSintatico(analisadorLexico.tabela);
+            analisadorSintatico.fazerAnalise();
+            String codigoIntemediario = analisadorSintatico.getCodigoIntemediario();
+
+            String filenameOut = filename.replace(".lalg",".blalg");
+            Files.write(Paths.get(filenameOut),codigoIntemediario.getBytes());
+
+        } catch (AnaliseLexicaException | AnaliseSintaticaException e ) {
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
